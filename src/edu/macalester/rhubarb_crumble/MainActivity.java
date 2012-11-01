70a40cf5dcaff68e2675a7c6d170db19caa4cdc1
Mel <Mel@141.140.177.57>
2012-11-01 14:13:29 -0500

    Completed MainActivity - added onclicklisteners for the three main buttons, added onClick() function to start new activities when each button is pressed. Added temporary main.xml template with button ids and button names to strings.xml

--- src/edu/macalester/rhubarb_crumble/MainActivity.java
+++ src/edu/macalester/rhubarb_crumble/MainActivity.java
@@ -1,18 +1,41 @@
 package edu.macalester.rhubarb_crumble;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity implements OnClickListener {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /** Set onClickListeners for the three buttons on the main screen. */
+        View currencyButton = findViewById(R.id.currency_button);
+        currencyButton.setOnClickListener(this);
+        
+        View unitsButton = findViewById(R.id.units_button);
+        unitsButton.setOnClickListener(this);
+        
+        View calculatorButton = findViewById(R.id.calculator_button);
+        calculatorButton.setOnClickListener(this);
     }
-    String s;
-    /** Test comment. **/
-    /**Hello, world!. **/
+    
+    public void onClick(View v) {
+    	switch(v.getId()) {
+    	case R.id.currency_button:
+    		startActivity(new Intent(this, CurrencyConverterActivity.class));
+    		break;
+    	case R.id.units_button:
+    		startActivity(new Intent(this, UnitCategoryChooserActivity.class));
+    		break;
+    	case R.id.calculator_button:
+    		startActivity(new Intent(this, CalculatorActivity.class));
+    	}
+    }
 }
