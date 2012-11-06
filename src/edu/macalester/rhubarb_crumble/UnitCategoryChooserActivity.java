f38920b25d1401cc092d5434484406e5cd38cbb3
samhorlbeck <shorlbec@macalester.edu>
2012-11-05 19:01:25 -0600

    Initial design of unit_category_chooser activity

--- src/edu/macalester/rhubarb_crumble/UnitCategoryChooserActivity.java
+++ src/edu/macalester/rhubarb_crumble/UnitCategoryChooserActivity.java
@@ -2,6 +2,12 @@ package edu.macalester.rhubarb_crumble;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.content.Intent;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.view.animation.AnimationUtils;
+import android.view.View.OnClickListener;
 
 public class UnitCategoryChooserActivity extends Activity {
 
@@ -9,5 +15,10 @@ public class UnitCategoryChooserActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.unit_category_chooser);
+		View additionalUnitsLabel = findViewById(R.id.additional_units_label);
+		RotateAnimation animate = (RotateAnimation)AnimationUtils.loadAnimation(this, R.animator.rotate90);
+		animate.setFillAfter(true);
+		additionalUnitsLabel.setAnimation(animate);
+		
 	}
 }
