58ea213506841b019d2e9321db349666213770df
Eric Biggers <ebiggers3@gmail.com>
2012-11-05 21:28:06 -0600

    Unit.java
    - Change endlines to '\n'
    - Add some comments
    - Rename some variables

--- src/edu/macalester/rhubarb_crumble/Unit.java
+++ src/edu/macalester/rhubarb_crumble/Unit.java
@@ -4,50 +4,63 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
 
+// Represents a unit that's available in the unit converter.
 public class Unit {
-	
-	String localizedName;
-	String abbreviatedName;
-	double normValue;
-	int visibility;
-	
+
+	// The name of the unit that will be displayed to the user.
+	private String localizedName;
+
+	// The abbreviation of the unit that will be displayed to the user.
+	private String abbreviatedName;
+
+	// The value of the unit, relative to some standard unit that measures the
+	// same thing (e.g. lengths are all relative to meters)
+	private double normalizedValue;
+
+	// The higher this number, the more uncommon the unit is.
+	private int visibilityLevel;
+
 	private static String TAG = "CrumbleConverterUnit";
-	
-	public Unit(Context context, String name, double normalizedValue, int visibilityLevel) {
+
+	public Unit(Context context, String identifier_name, double normalizedValue,
+				int visibilityLevel)
+	{
+		Resources resources = context.getResources();
+		String pkgname = context.getPackageName();
 		try {
-			int nameId = context.getResources().getIdentifier(name, "string", "edu.macalester.rhubarb_crumble");
-			localizedName = context.getResources().getString(nameId);
+			int nameId = resources.getIdentifier(identifier_name, "string", pkgname);
+			this.localizedName = resources.getString(nameId);
 		} catch (Resources.NotFoundException e) {
-			Log.e(TAG, "Can't find unit: " + name, e);
-			localizedName = name;
+			Log.e(TAG, "Can't find unit: " + identifier_name, e);
+			this.localizedName = identifier_name;
 		}
-		
+
 		try {
-			int abbrevId = context.getResources().getIdentifier(name + ".abbrev", "string", "edu.macalester.rhubarb_crumble");
-			abbreviatedName = context.getResources().getString(abbrevId);
+			int abbrevId = resources.getIdentifier(identifier_name + ".abbrev",
+												   "string", pkgname);
+			this.abbreviatedName = resources.getString(abbrevId);
 		} catch (Resources.NotFoundException e) {
-			Log.e(TAG, "Can't find unit abbreviation: " + name, e);
-			abbreviatedName = name;
+			Log.e(TAG, "Can't find unit abbreviation: " + identifier_name, e);
+			this.abbreviatedName = identifier_name;
 		}
-		
-		normValue = normalizedValue;
-		visibility = visibilityLevel;
+
+		this.normalizedValue = normalizedValue;
+		this.visibilityLevel = visibilityLevel;
 	}
-	
+
 	public String getLocalizedName() {
 		return localizedName;
 	}
-	
+
 	public String getLocalizedAbbreviation() {
 		return abbreviatedName;
 	}
-	
+
 	public double getNormalizedValue() {
-		return normValue;
-	}
-	
-	public int visibility() {
-		return visibility;
+		return normalizedValue;
 	}
 
+	public int getVisibilityLevel() {
+		return visibilityLevel;
+	}
 }
