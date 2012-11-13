de25e147a6422f93250165d29ddeabfd97c72289
Mel <Mel@141.140.179.155>
2012-11-12 20:18:47 -0600

    Created Metrix Prefix activity and xml file. Updated strings.xml to include needed strings.

---
+++ src/edu/macalester/rhubarb_crumble/MetrixPrefixesListActivity.java
@@ -0,0 +1,16 @@
+package edu.macalester.rhubarb_crumble;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MetrixPrefixesListActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Set the content view to metric_prefixes.xml
+		setContentView(R.layout.metric_prefixes_list);
+	}
+
+}
